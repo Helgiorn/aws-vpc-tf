05bfc4a3a2bd1251a7adfd4523067a2c1b510c25
Helgi Gunnarsson <helgiorn@gmail.com>
2022-04-27 07:47:32 +0200

    Initial commit

---
+++ variables.tf
@@ -0,0 +1,26 @@
+variable "region" {
+  description = "aws region"
+  default     = "eu-west-1"
+}
+
+variable "environment" {
+  description = "The Deployment environment"
+}
+
+//Networking
+variable "vpc_cidr" {
+  description = "The CIDR block of the vpc"
+  default     = ["10.0.0.0/16"]
+}
+
+variable "public_subnets_cidr" {
+  type        = list(any)
+  description = "The CIDR block for the public subnet"
+  default     = ["10.0.1.0/24"]
+}
+
+variable "private_subnets_cidr" {
+  type        = list(any)
+  description = "The CIDR block for the private subnet"
+  default     = ["10.0.10.0/24"]
+}
